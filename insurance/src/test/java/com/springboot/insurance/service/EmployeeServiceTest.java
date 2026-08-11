3a7c147d6df5cad36c2fb22c2cd34de37bfcfe31
KEERTHANA.D <keercs062@rmkcet.ac.in>
2026-08-11 15:14:33 +0530

    Increase employee service test coverage

--- insurance/src/test/java/com/springboot/insurance/service/EmployeeServiceTest.java
+++ insurance/src/test/java/com/springboot/insurance/service/EmployeeServiceTest.java
@@ -1,12 +1,10 @@
 package com.springboot.insurance.service;
 
-
 import com.springboot.insurance.dto.request.EmployeeRequestDto;
 import com.springboot.insurance.enums.EmployeeRole;
 import com.springboot.insurance.enums.Role;
 import com.springboot.insurance.exception.ResourceNotFoundException;
 import com.springboot.insurance.model.Employee;
-import com.springboot.insurance.model.PolicyHolder;
 import com.springboot.insurance.model.User;
 import com.springboot.insurance.repository.EmployeeRepository;
 import com.springboot.insurance.repository.UserRepository;
@@ -20,7 +18,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -53,7 +50,6 @@ public class EmployeeServiceTest {
     @BeforeEach
     public void init() {
 
-
         user1 = new User(
                 1L,
                 "john@gmail.com",
@@ -63,7 +59,6 @@ public class EmployeeServiceTest {
                 true
         );
 
-
         employee1 = new Employee(
                 1L,
                 "John Doe",
@@ -71,8 +66,6 @@ public class EmployeeServiceTest {
                 true,
                 user1
         );
-
-
 
         user2 = new User(
                 2L,
@@ -83,8 +76,7 @@ public class EmployeeServiceTest {
                 true
         );
 
-
-        employee2= new Employee(
+        employee2 = new Employee(
                 2L,
                 "Jack Doe",
                 EmployeeRole.INSURANCE_MANAGER,
@@ -101,7 +93,6 @@ public class EmployeeServiceTest {
                 true
         );
 
-
         employee3 = new Employee(
                 3L,
                 "Jacky Doe",
@@ -113,7 +104,8 @@ public class EmployeeServiceTest {
     }
 
     @Test
-    public void addTest(){
+    public void addTest() {
+
         when(passwordEncoder.encode("john123")).thenReturn("encodedPass");
         when(userRepository.save(any(User.class))).thenReturn(user1);
         when(employeeRepository.save(any(Employee.class))).thenReturn(employee1);
@@ -128,17 +120,36 @@ public class EmployeeServiceTest {
 
         employeeService.add(dto);
 
-        ArgumentCaptor<Employee> employeeCaptor = ArgumentCaptor.forClass(Employee.class);
-        verify(employeeRepository, times(1)).save(employeeCaptor.capture());
+        ArgumentCaptor<Employee> employeeCaptor =
+                ArgumentCaptor.forClass(Employee.class);
 
-        Assertions.assertEquals(dto.name(), employeeCaptor.getValue().getName());
+        verify(employeeRepository, times(1))
+                .save(employeeCaptor.capture());
 
-        Assertions.assertEquals(dto.employeeRole(), employeeCaptor.getValue().getEmployeeRole());
+        Assertions.assertEquals(
+                dto.name(),
+                employeeCaptor.getValue().getName()
+        );
 
-        Assertions.assertEquals(dto.username(), employeeCaptor.getValue().getUser().getUsername());
+        Assertions.assertEquals(
+                dto.employeeRole(),
+                employeeCaptor.getValue().getEmployeeRole()
+        );
 
-        Assertions.assertEquals(Role.EMPLOYEE, employeeCaptor.getValue().getUser().getRole());
-        Assertions.assertEquals("encodedPass", employeeCaptor.getValue().getUser().getPassword());
+        Assertions.assertEquals(
+                dto.username(),
+                employeeCaptor.getValue().getUser().getUsername()
+        );
+
+        Assertions.assertEquals(
+                Role.EMPLOYEE,
+                employeeCaptor.getValue().getUser().getRole()
+        );
+
+        Assertions.assertEquals(
+                "encodedPass",
+                employeeCaptor.getValue().getUser().getPassword()
+        );
 
     }
 
@@ -146,9 +157,16 @@ public class EmployeeServiceTest {
     @Test
     public void getByIdPresent() {
 
-        when(employeeRepository.fetchById(1L)).thenReturn(Optional.of(employee1));
-        Assertions.assertEquals("John Doe", employeeService.getById(1L).name());
-        verify(employeeRepository, times(1)).fetchById(1L);
+        when(employeeRepository.fetchById(1L))
+                .thenReturn(Optional.of(employee1));
+
+        Assertions.assertEquals(
+                "John Doe",
+                employeeService.getById(1L).name()
+        );
+
+        verify(employeeRepository, times(1))
+                .fetchById(1L);
 
     }
 
@@ -156,41 +174,162 @@ public class EmployeeServiceTest {
     @Test
     public void getByIdAbsent() {
 
+        when(employeeRepository.fetchById(10L))
+                .thenReturn(Optional.empty());
 
-        when(employeeRepository.fetchById(10L)).thenReturn(Optional.empty());
-
-        Assertions.assertEquals("Employee id invalid",
+        Assertions.assertEquals(
+                "Employee id invalid",
                 Assertions.assertThrows(
                         ResourceNotFoundException.class,
                         () -> employeeService.getById(10L)
                 ).getMessage()
         );
 
-        verify(employeeRepository, times(1)).fetchById(10L);
+        verify(employeeRepository, times(1))
+                .fetchById(10L);
 
     }
+
 
     @Test
     public void getByEmployeeRoleTest() {
 
-        List<Employee> employeeList = List.of(employee1, employee2);
+        List<Employee> employeeList =
+                List.of(employee1, employee2);
 
-        when(employeeRepository.getByEmployeeRole(EmployeeRole.CLAIM_MANAGER)).thenReturn(employeeList);
+        when(employeeRepository.getByEmployeeRole(
+                EmployeeRole.CLAIM_MANAGER
+        )).thenReturn(employeeList);
 
-        Assertions.assertEquals(2,employeeService.getByEmployeeRole(EmployeeRole.CLAIM_MANAGER).size());
+        Assertions.assertEquals(
+                2,
+                employeeService
+                        .getByEmployeeRole(EmployeeRole.CLAIM_MANAGER)
+                        .size()
+        );
 
-        verify(employeeRepository, times(1)).getByEmployeeRole(EmployeeRole.CLAIM_MANAGER);
+        verify(employeeRepository, times(1))
+                .getByEmployeeRole(EmployeeRole.CLAIM_MANAGER);
 
     }
+
+
+    @Test
+    public void getByUserTest() {
+
+        when(employeeRepository.findByUser(user1))
+                .thenReturn(Optional.of(employee1));
+
+        Assertions.assertEquals(
+                employee1,
+                employeeService.getByUser(user1)
+        );
+
+        verify(employeeRepository, times(1))
+                .findByUser(user1);
+
+    }
+
+
+    @Test
+    public void getByUserInvalidTest() {
+
+        when(employeeRepository.findByUser(user1))
+                .thenReturn(Optional.empty());
+
+        Assertions.assertEquals(
+                "Employee not found",
+                Assertions.assertThrows(
+                        ResourceNotFoundException.class,
+                        () -> employeeService.getByUser(user1)
+                ).getMessage()
+        );
+
+        verify(employeeRepository, times(1))
+                .findByUser(user1);
+
+    }
+
+
+    @Test
+    public void deleteTest() {
+
+        employee1.setActive(true);
+
+        when(employeeRepository.findById(1L))
+                .thenReturn(Optional.of(employee1));
+
+        when(employeeRepository.save(employee1))
+                .thenReturn(employee1);
+
+        employeeService.delete(1L);
+
+        Assertions.assertFalse(employee1.isActive());
+
+        verify(employeeRepository, times(1))
+                .findById(1L);
+
+        verify(employeeRepository, times(1))
+                .save(employee1);
+
+    }
+
+
+    @Test
+    public void deleteTestForInactiveEmployee() {
+
+        employee1.setActive(false);
+
+        when(employeeRepository.findById(1L))
+                .thenReturn(Optional.of(employee1));
+
+        when(employeeRepository.save(employee1))
+                .thenReturn(employee1);
+
+        employeeService.delete(1L);
+
+        Assertions.assertTrue(employee1.isActive());
+
+        verify(employeeRepository, times(1))
+                .findById(1L);
+
+        verify(employeeRepository, times(1))
+                .save(employee1);
+
+    }
+
+
+    @Test
+    public void deleteInvalidIdTest() {
+
+        when(employeeRepository.findById(5L))
+                .thenReturn(Optional.empty());
+
+        Assertions.assertEquals(
+                "Employee Id invalid",
+                Assertions.assertThrows(
+                        ResourceNotFoundException.class,
+                        () -> employeeService.delete(5L)
+                ).getMessage()
+        );
+
+        verify(employeeRepository, times(1))
+                .findById(5L);
+
+        verify(employeeRepository, never())
+                .save(any());
+
+    }
+
 
     @Test
     public void deleteIdTest() {
 
-        when(employeeRepository.findById(1L)).thenReturn(Optional.of(employee1));
-//        doNothing().when(policyHolderRepository).save(policyHolder1);
+        when(employeeRepository.findById(1L))
+                .thenReturn(Optional.of(employee1));
+
         when(employeeRepository.save(employee1))
                 .thenReturn(employee1);
-
 
         employeeService.deleteId(1L);
 
@@ -199,26 +338,32 @@ public class EmployeeServiceTest {
 
     }
 
+
     @Test
     public void deleteIdInvalidId() {
 
+        when(employeeRepository.findById(5L))
+                .thenReturn(Optional.empty());
 
-        when(employeeRepository.findById(5L)).thenReturn(Optional.empty());
-
-        Assertions.assertEquals("Employee Id invalid",
-
+        Assertions.assertEquals(
+                "Employee Id invalid",
                 Assertions.assertThrows(
                         ResourceNotFoundException.class,
-                        () ->employeeService.deleteId(5L)
+                        () -> employeeService.deleteId(5L)
                 ).getMessage()
         );
-        verify(employeeRepository,never()).save(any());
+
+        verify(employeeRepository, never())
+                .save(any());
 
     }
 
+
     @Test
-    public void updateTest(){
-        when(employeeRepository.findById(10L)).thenReturn(Optional.of(employee1));
+    public void updateTest() {
+
+        when(employeeRepository.findById(10L))
+                .thenReturn(Optional.of(employee1));
 
         EmployeeRequestDto dto = new EmployeeRequestDto(
                 "John J. Doe",
@@ -227,18 +372,24 @@ public class EmployeeServiceTest {
                 "",
                 true
         );
-        // I am making an actual call, this must ensure that , fetchById and save method both get called exactly ONCE
-        employeeService.update(10L , dto);
 
-        verify(employeeRepository, times(1)).findById(10L);
-        verify(employeeRepository, times(1)).save(employee1);
+        employeeService.update(10L, dto);
+
+        verify(employeeRepository, times(1))
+                .findById(10L);
+
+        verify(employeeRepository, times(1))
+                .save(employee1);
 
     }
+
 
     @Test
     public void updateTestForInvalidEmployeeId() {
 
-        when(employeeRepository.findById(11L)).thenReturn(Optional.empty());
+        when(employeeRepository.findById(11L))
+                .thenReturn(Optional.empty());
+
         EmployeeRequestDto dto = new EmployeeRequestDto(
                 "John J. Doe",
                 EmployeeRole.CLAIM_MANAGER,
@@ -246,17 +397,59 @@ public class EmployeeServiceTest {
                 "",
                 true
         );
-        Assertions.assertEquals("Employee Id invalid",
-                Assertions.assertThrows(ResourceNotFoundException.class,
-                                ()->employeeService.update(11L , dto) )
-                        .getMessage()
+
+        Assertions.assertEquals(
+                "Employee Id invalid",
+                Assertions.assertThrows(
+                        ResourceNotFoundException.class,
+                        () -> employeeService.update(11L, dto)
+                ).getMessage()
         );
 
-        verify(employeeRepository, times(1)).findById(11L);
-        verify(employeeRepository, times(0)).save(employee1);
+        verify(employeeRepository, times(1))
+                .findById(11L);
+
+        verify(employeeRepository, times(0))
+                .save(employee1);
+
     }
 
 
+    @Test
+    public void getAllTest() {
 
+        List<Employee> employeeList =
+                List.of(employee1, employee2, employee3);
+
+        when(employeeRepository.findAll())
+                .thenReturn(employeeList);
+
+        Assertions.assertEquals(
+                3,
+                employeeService.getAll().size()
+        );
+
+        verify(employeeRepository, times(1))
+                .findAll();
+
+    }
+
+
+    @Test
+    public void getAllEmptyTest() {
+
+        when(employeeRepository.findAll())
+                .thenReturn(List.of());
+
+        Assertions.assertEquals(
+                0,
+                employeeService.getAll().size()
+        );
+
+        verify(employeeRepository, times(1))
+                .findAll();
+
+    }
 
 }
+
